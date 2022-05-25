27965eb0fff96f056d62e75d941d88f3925b83b8
Eric Sun <ericmsun@gmail.com>
2022-05-24 20:16:33 -0400

    deleted index.js from component folder and imported components to App.js, fixed className props to be more readable, fixed style issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Home from "./components";
+
 import React from "react";
+import Navigation from './components/Navbar';
+import About from "./components/About";
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+
 import './App.css';
 
 function App() {
   return (
-  <div className='main'>
-   <Home />
-  </div>
+    <div className='app-container'>
+      <Navigation />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </div>
   );
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,27 +6,28 @@ import { projects } from "../assets/data";
 
 function Projects() {
   return (
-    <Container className="project-container p-5 my-4" id="projects">
+    <Container className="main-project-container p-5 my-4" id="projects">
       <Container>
-        <h1>Apps I've Built</h1>
-        <p>Here are some examples of apps I have built using different technologies !</p>
-      </Container>
-      <Container >
-        {projects.map((project) => (
-          <Card className="card-container p-5 my-4">
-            <Card.Body className="flex">
-              <Card.Title>{project.title}</Card.Title>
-              <Card.Img src={project.image}></Card.Img>
-              <Card.Subtitle>{project.subtitle}</Card.Subtitle>
-              <Card.Text>
-                {project.description}
-              </Card.Text>
-              <Card.Link href={project.link}>Link</Card.Link>
-              <Card.Link href={project.github}>Github</Card.Link>
-            </Card.Body>
-          </Card>
-        ))}
-
+        <Container className="project-section-header">
+          <h1>Apps I've Built</h1>
+          <p>Here are some examples of apps I have built using different technologies !</p>
+        </Container>
+        <Container className="card-container">
+          {projects.map((project) => (
+            <Card className=" card-body-container p-5 my-4">
+              <Card.Body>
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Img src={project.image}></Card.Img>
+                <Card.Subtitle>{project.subtitle}</Card.Subtitle>
+                <Card.Text>
+                  {project.description}
+                </Card.Text>
+                <Card.Link href={project.link}>Link</Card.Link>
+                <Card.Link href={project.github}>Github</Card.Link>
+              </Card.Body>
+            </Card>
+          ))}
+        </Container>
       </Container>
     </Container>
   )
