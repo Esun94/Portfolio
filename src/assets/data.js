1848c4ed1e057bbc197c89bb117e44e6fde3fc75
Eric Sun <ericmsun@gmail.com>
2022-06-19 13:54:36 -0400

    saving projectsTail container for imessage bubble

--- src/assets/data.js
+++ src/assets/data.js
@@ -36,5 +36,6 @@ export const skills = [
     'Express',
     'jQuery',
     'SQL',
-    'MySQL'
+    'MySQL',
+    // 'Python'
 ];
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,13 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Projects() {
   return (
     <Container className="main-project-container " id="projects">
-      <Container className="project-section-header text-center ">
-        <h1>Apps I've Built</h1>
-        <p>Here are some examples of apps I have built using different technologies !</p>
+      <Container className="project-section-header">
+        <h1 className="projects-intro">Apps I've Built</h1>
+        <p className="projects-desc">Here are some examples of apps I have built using different technologies !</p>
       </Container>
       <Container className="projects-card-container">
         {projects.map((project) => (
-          <Card.Body className="project-container" col-md-3 mx-1 my-2>
+          <Card.Body className="project-container" >
             <Card.Img className="mb-3" src={project.image}></Card.Img>
             <Card.Title className="text-center mb-4">{project.title}</Card.Title>
             <Card.Subtitle className="text-center mt-3">{project.subtitle}</Card.Subtitle>
@@ -24,6 +24,8 @@ function Projects() {
             <Card.Link href={project.github}>Github</Card.Link>
           </Card.Body>
         ))}
+      </Container>
+      <Container className="projects-tail">
       </Container>
     </Container>
   )
